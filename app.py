b0c544dfa03f8f4bf387815185d452d1a9985b6e
jstone-saw <155635658+jstone-saw@users.noreply.github.com>
2025-04-02 15:01:28 +0100

    More Updates

--- app.py
+++ app.py
@@ -1,17 +1,4 @@
-# Add this at the very top of your app.py file
 import streamlit as st
-import sys
-import subprocess
-
-# Install required packages
-required_packages = ['matplotlib', 'seaborn', 'pandas']
-for package in required_packages:
-    try:
-        __import__(package)
-    except ImportError:
-        subprocess.check_call([sys.executable, "-m", "pip", "install", package])
-
-# Now import what you need from these packages
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
